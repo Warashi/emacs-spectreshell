a49e26eff79d1a19e04e2adafd2449e431de101c
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-07-18 12:02:50 +0900

    build: add nix flake files

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+    };
+
+    # Used for shell.nix
+    flake-compat = {
+      url = "github:NixOS/flake-compat";
+      flake = false;
+    };
+
+    systems = {
+      url = "github:nix-systems/default";
+      flake = false;
+    };
+
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-compat.follows = "flake-compat";
+        systems.follows = "systems";
+      };
+    };
+
+    zon2nix = {
+      url = "github:nix-community/zon2nix";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        zig-overlay.follows = "zig-overlay";
+      };
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      zig-overlay,
+      zon2nix,
+      ...
+    }@inputs:
+    let
+      platforms = nixpkgs.lib.attrNames zig-overlay.packages;
+      forAllPlatforms = f: nixpkgs.lib.genAttrs platforms (s: f nixpkgs.legacyPackages.${s});
+    in
+    {
+      devShells = forAllPlatforms (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nixfmt
+          ];
+        };
+      });
+    };
+}
