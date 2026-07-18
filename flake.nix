c82aa360e87f061462779d108f3181563e985380
Shinnosuke Sawada-Dazai <3600530+Warashi@users.noreply.github.com>
2026-07-18 12:52:24 +0900

    chore: remove zig-overlay

--- flake.nix
+++ flake.nix
@@ -9,38 +9,27 @@
       url = "github:NixOS/flake-compat";
       flake = false;
     };
-
-    systems = {
-      url = "github:nix-systems/default";
-      flake = false;
-    };
-
-    zig-overlay = {
-      url = "github:mitchellh/zig-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-compat.follows = "flake-compat";
-        systems.follows = "systems";
-      };
-    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      zig-overlay,
       ...
     }@inputs:
     let
-      platforms = nixpkgs.lib.attrNames zig-overlay.packages;
-      forAllPlatforms = f: nixpkgs.lib.genAttrs platforms (s: f nixpkgs.legacyPackages.${s});
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (s: f nixpkgs.legacyPackages.${s});
     in
     {
-      formatter = forAllPlatforms (pkgs: pkgs.nixfmt-tree);
-      devShells = forAllPlatforms (pkgs: {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-tree);
+      devShells = forAllSystems (pkgs: {
         default = pkgs.callPackage ./nix/devShell.nix {
-          zig = zig-overlay.packages.${pkgs.stdenv.hostPlatform.system}."0.16.0";
+          zig = pkgs.zig_0_16;
         };
       });
     };
